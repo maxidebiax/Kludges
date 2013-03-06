2474ca29d17103fd89f4f1feb8d8068873b5815c
romain hennuyer <romainhk@gmail.com>
2013-03-06 16:27:41 +0100

    separation des traitements ; operation sur des set

--- kwartz/ajout_profs_depuis_pronote/profs-kwartz.py
+++ kwartz/ajout_profs_depuis_pronote/profs-kwartz.py
@@ -20,6 +20,10 @@ Fichier > Autres imports/exports > Exporter un fichier texte
 * Format d'export > Charger > export_classe-profs.xml
 * Puis Exporter...
 
+=== Sorties ===
+* 3 fichiers textes à importer dans kwartz
+* sur la sortie standard, les doutes et un résumé des modifications
+
 Attention, Pronote exporte en encodage utf-16-le (aller savoir pourquoi) ; il faut le convertir en utf-8 avant usage. Sous linux, il suffit d'exécuter la commande :
 > iconv -f UTF-16 -t UTF-8 MON_EXPORT_PRONOTE.txt > EXPORT_PRONOTE.txt
 """
@@ -27,7 +31,7 @@ Attention, Pronote exporte en encodage utf-16-le (aller savoir pourquoi) ; il fa
 if len(sys.argv) != 4:
     """
     print(u"!!! Ce programme nécessite trois paramètres : !!!\n" + \
-    	  u"* le fichier d'export de kwartz\n" + \
+          u"* le fichier d'export de kwartz\n" + \
           u"* l'export classes-profs issu de pronote\n" + \
           u"* le nom du groupe kwartz des profs\n" + \
           u"\nexemple: python profs-kwartz.py export EXP_PRONOTE.txt GroupeProf")
@@ -45,7 +49,6 @@ OUTmodif = codecs.open(u'Amodifier.txt', 'w', encoding='utf-8')
 OUTajout = codecs.open(u'Aajouter.txt', 'w', encoding='utf-8')
 OUTsuppr = codecs.open(u'Asupprimer.txt', 'w', encoding='utf-8')
 
-
 def les_classes(table):
     # Génère la chaîne "groupe"
     c = []
@@ -57,28 +60,30 @@ def les_classes(table):
         c.append(suffixe + unicode(b).replace('-', ''))
     return u' '.join(c)
 
-profs = {} # dico des profs
+### Lecture des données ###
+profsPronote = {} # profs connus dans Pronote
 # format : 1S;M. DOE JOHN, Mme TARTELATIE FABIENNE
 REprof = re.compile('M(\.|me)\s+(.+)', re.IGNORECASE|re.UNICODE)
 for ligne in EXPpronote:
     a = ligne.split(';')
     classe = a[0].lower()
-    if len(a) == 2 and classe != 'CLASSES' and len(classe) > 0:
+    if classe != 'CLASSES' and len(classe) > 0:
         for i in a[1].split(','): # Plusieurs profs avec la même matière dans la même classe
             n = REprof.search(i)
             if n:
                 nom = unicode(n.group(2).strip().replace("'", '').upper())
-                # TODO: si le nom est composé
-                if nom in profs:
-                    if classe not in profs[nom]:
-                        profs[nom].append(classe)
-                else:
-                    profs[nom] = [ classe ]
+                classe = classe.replace("-","").replace("bps", "bsp")
+                if classe[0].isdigit():
+                    classe = u"c"+classe
+                if classe[:3] != "ufa":
+                    if nom in profsPronote:
+                        if classe not in profsPronote[nom]:
+                            profsPronote[nom].append(classe)
+                    else:
+                        profsPronote[nom] = [ classe ]
 
-profsKwartz={}
-
-ajouter = profs.keys() # liste des profs à créer
-# Modification et suppression des comptes
+profsKwartz={} # profs connus dans Kwartz
+infos={} # donnée supplémentaire pour les profs
 # format : JOHN;Doe;profs;doe.john;doe.john;;;profs www;profs www;;;;;;<LOCAL>;Professeur;
 REkwartz = re.compile('^(.+);(.+);%s;(.+?);[^;]*;[^;]*;[^;]*;[^;]*;(.*?);.*' % groupe_profs, re.IGNORECASE|re.LOCALE|re.MULTILINE)
 for ligne in EXPkwartz:
@@ -90,40 +95,67 @@ for ligne in EXPkwartz:
         j = "{nom} {pre}".format(nom=nom, pre=prenom)
         login = m.group(3)
         classes = m.group(4).split(' ')
-        profsKwartz[nom] = classes
-        if j in profs:
-            # Reprendre et modifier la ligne prof
-            a = ligne.split(';')
-            a[0] = nom.upper()
-            a[1] = prenom.capitalize()
-            a[7] = u''
-            a[8] = les_classes(profs[j])
-            OUTmodif.write(u';'.join(a))
-            # Retirer ce prof de la liste des profs à ajouter
-            ajouter.remove(j)
-        else:
-            # Déplacement dans le groupe "anciens"
-            OUTsuppr.write(u'{nom};{prenom};anciens;{login};;;;;;;;;;;<LOCAL>;;\r\n'.format(nom=nom, prenom=prenom, login=login))
-            # TODO: faire une recherche approfondie sur les variations classique d'orthographe
+        if len(classes) > 0:
+            profsKwartz[j] = classes
+        a = ligne.split(';')
+        a[0] = nom.upper()
+        a[1] = prenom.capitalize()
+        a[3] = login
+        a[7] = u''
+        a[8] = u' '.join(classes)
+        infos[j] = a
 
-EXPkwartz.close()
-EXPpronote.close()
-OUTmodif.close()
-OUTsuppr.close()
-
-# Créer les profs manquants
-for a in ajouter:
+### Traitement ###
+# Format d'une ligne d'importation Kwartz
+enr = u'{nom};{prenom};{groupe};{login};{login};;;;{classes};;;;;;<LOCAL>;Professeur;\r\n'
+# À ajouter
+Aajouter = list( set(profsPronote) - set(profsKwartz) )
+Averif = {}
+for a in sorted(Aajouter):
     b = a.split(' ')
     nom = b[0]
-    if len(b) > 1:
-        prenom = b[1]
+    prenom = b[1].capitalize()
+    login = (prenom+'.'+nom).lower().replace(' ','')
+    classes = les_classes(profsPronote[a])
+    if len(b) > 2:
+        #Nom composé !
+        Averif[nom] = enr.format(nom=nom, prenom=prenom, groupe=groupe_profs, login=login, classes=classes)
     else:
-        print(u'Pas de prénom pour : {c}'.format(c=a))
-    login = u'{prenom}.{nom}'.format(nom=nom.lower(), prenom=prenom.lower())
-    classe = les_classes(profs[a])
-    OUTajout.write(u'{nom};{prenom};profs;{login};{login};;;;{classe};;;;;;<LOCAL>;Professeur;\r\n'.format(nom=nom, prenom=prenom, login=login, classe=classe))
+        OUTajout.write(enr.format(nom=nom, prenom=prenom, groupe=groupe_profs, login=login, classes=classes))
+
+# À supprimer ?
+#Comptes Kwartz qui sont inconnus dans Pronote
+#Nécessairement plein de faux positifs
+Asuppr = list( set(profsKwartz) - set(profsPronote) )
+for a in sorted(Asuppr):
+    nom = infos[a][0]
+    if nom in Averif.keys():
+        # Nom déjà vérifier => proposition de correction
+        Averif[nom] = Averif[nom]+"> "+enr.format(nom=nom, prenom=infos[a][1], groupe=u'anciens', login=infos[a][3], classes=u'')
+    else:
+        OUTsuppr.write(enr.format(nom=nom, prenom=infos[a][1], groupe=u'anciens', login=infos[a][3], classes=u''))
+if len(Averif) > 1:
+    print(u"### Comptes dont le nom est composé ###")
+    print(u"=> Il faut vérifier l'orthographe du nom ou la correspondance kwartz/pronote, puis déplacer dans le bon fichier\n")
+    for k, av in Averif.items():
+        print(av)
+
+# À modifier
+Amodifier = list( set(profsKwartz) & set(profsPronote) )
+print(u"### Résumé des changements d'affectation ###")
+for a in sorted(Amodifier):
+    # Comparaison des classes déclarées
+    c = set(profsKwartz[a])^set(profsPronote[a])
+    if len(c) > 0:
+        ajout = ' +'.join(set(profsPronote[a])-set(profsKwartz[a]))
+        retrait = ' -'.join(set(profsKwartz[a])-set(profsPronote[a]))
+        print("{prof}: +{ajout} // -{retrait}".format(prof=a, ajout=ajout, retrait=retrait))
+        nom = infos[a][0]
+        prenom = infos[a][1]
+        login = infos[a][3]
+        classes = les_classes(profsPronote[a])
+        OUTmodif.write(enr.format(nom=nom, prenom=prenom, groupe=groupe_profs, login=login, classes=classes))
 
 OUTajout.close()
-
-print(set(profsKwartz) - set(profs))
-
+OUTsuppr.close()
+OUTmodif.close()
