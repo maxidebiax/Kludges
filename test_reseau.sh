eb24dd2f7242ba7b50832608de7cb678bd268321
romain hennuyer <romainhk@gmail.com>
2013-04-05 14:57:25 +0200

    en paramètre : la liste des ip

--- test_reseau.sh
+++ test_reseau.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Test la présence d'une liste de machines sur le réseau
+# Paramètre 1 : nom du fichier comptenant la liste des adresses
 
-ADRESSES="test_reseau.txt"
+ADRESSES=$1
 # Fichier contenant les adresses à tester, une IP par ligne
 
 function tester()
@@ -9,10 +10,11 @@ function tester()
     hote=`host $1|cut -d" " -f5`
     local a=`ping -c 1 -q $1|grep -G "[^0] received"`
     if [ ${#a} -gt 0 ];then
-        echo -e "$1\t($hote) ...\tOK"
+        #echo -e "$1\t($hote) ...\tOK"
+        echo -e "$hote\t($1) ...\tOK"
         return 0
     fi
-    echo "Impossible de joindre $1 ($hote)"
+    echo "$1 : impossible de joindre $hote"
 }
 
 while read line
