60b7cf1c95daa11e6ea5cb5797db206e1204afd2
romain hennuyer <romainhk@gmail.com>
2013-06-07 11:28:32 +0200

    maj domaine

--- kwartz/decocher_inscription_auto.js
+++ kwartz/decocher_inscription_auto.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name        Décocher inscription auto
-// @namespace   https://kwartz-server.jeanmoulin.fr
+// @namespace   https://kwartz-server/
 // @description Décoche automatiquement les ordinateurs lors de l'inscription automatique de poste dans Kwartz
-// @include     https://kwartz-server.jeanmoulin.fr:9999/hostauto.cgi
+// @include     https://kwartz-server:9999/hostauto.cgi
+// @grant       none
 // @version     1
 // ==/UserScript==
 //var b = window.frames["MAIN"].document.getElementsByName("leasehost");
