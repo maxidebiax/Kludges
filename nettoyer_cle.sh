db03ef40b76a83fb5b549922ed8006a4dca92233
romainhk <romainhk@gmail.com>
2013-11-14 22:39:41 +0100

    passage par les mtools (plus de formattage)

--- nettoyer_cle.sh
+++ nettoyer_cle.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Script de nettoyage de clé pour le trojan iTunesHelper.vbe / .vbs
+#####  Notes  #####
+# Nécessite les mtools -> la clé doit être en fat
+# Nécessite l'accès au devices -> être root
 
 if [ ! $# -eq 1 ];then
     echo "ERREUR : Il faut préciser le point de montage de la clé !"
@@ -15,41 +19,28 @@ echo MEDIA $media
 echo DEVICE $dev
 read -p "Continuer ?" yn
 
-# Nettoyage
+# Nettoyage de la merde
 echo "# Nettoyage du virus sur la clé usb"
-cmd="rm -f"
-find $media -name "*.lnk" -exec $cmd {} \;
-find $media -name "*.vbs" -exec $cmd {} \;
-find $media -name "*.vbe" -exec $cmd {} \;
+cmd1="ls -l {}"
+cmd2="echo {}"
+#cmd2="rm -f {}"
+find $media \( -name "*.lnk" -o -name "*.vbs" -o -name "*.vbe" \) -exec $cmd1 \; -exec $cmd2 \;
 
-# Copie de sauvegarde
-echo "## Copie de la clé"
-tmp="/tmp/sauvegarde_cle"
-if [ ! -d $tmp ];then
-    mkdir $tmp
-else
-    read -p "$tmp plein ; procéder au nettoyage ? [O/n] " yn
-    case $yn in
-        [Nn]* )
-            rm -rf $tmp.old
-            mv $tmp $tmp.old ;;
-        * )
-            rm -rf $tmp ;;
-    esac
-fi
-cp -R $media $tmp
-
-# Formattage
-echo "### Formattage de la clé usb"
-read -p "Continuer ?" yn
-umount $media
-mkfs.vfat $dev
-
-# Restauration
-echo "#### Restoration du contenu de la clé usb"
-mount $dev
-mount=`mount | grep $dev`
-media=`echo $mount|cut -d' ' -f3`
-cp -R $tmp $media
+# Retrait des attributs hidden et system
+# mattrib : http://linlog.skepticats.com/entries/2007/10/26_1516/
+echo "## Paramétrage des mtools"
+mtoolsrc=~/.mtoolsrc
+end="1"
+echo "mtools_skip_check=1" > $mtoolsrc
+for lettre in a b c d e f g h i j k
+do
+    device="/dev/sd$lettre$end"
+    if [ -e $device ];then
+        echo "drive $lettre: file=\"$device\"" >> $mtoolsrc
+    fi
+done
+AA="${dev:7:1}:/"
+echo "### Restauration des attributs de fichiers normaux sur $AA"
+mattrib -s -h -/ $AA
 
 echo "##### Opération réussie !! #####"
